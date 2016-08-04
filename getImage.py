68a8cde54afcc0c0477bbc90e60f9892287197e5
omerkolkanat <omerkolkanat@gmail.com>
2016-08-05 00:12:10 +0300

    Get urls and save different db

---
+++ getImage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import json
+from pymongo import MongoClient
+clientFrom = MongoClient("mongodb://<username>:<password>@ds059145.mlab.com:59145/tweets") # Teacher's database
+dbFrom = clientFrom.tweets
+
+clientTo = MongoClient("mongodb://<username>:<password>@ds145355.mlab.com:45355/justurl") # Our db to get just JsonResponse object
+dbTo = clientTo.justurl
+
+cursor = dbFrom.JsonResponse.find() # get all tweets from teacher's database
+
+for document in cursor:
+   # get just JsonResponse object from Db
+    try:
+        text = document['user']['profile_image_url']
+        # text2 = document['entities']['media'][0]['media_url']
+    except:
+        continue
+    print text
+    result = dbTo.Url.insert_one({ "image" : text}) # add new database JsonResponse object.
+
